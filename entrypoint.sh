e09c5001baf9bd91d3a1cf589e19c91eb1ef6d42
murakamikaze <murakamikaze@validao.xyz>
2025-05-07 09:32:19 +0100

    Add scratch area for visor in /dev/shm/hl

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,9 @@ cron
 # Generate override gossip config
 python3 /usr/local/bin/generate_gossip_config.py
 
+# Give visor an exec-able scratch area for hl-node
+mkdir -p /dev/shm/hl && chmod 1777 /dev/shm/hl
+export TMPDIR=/dev/shm/hl          # visor honours TMPDIR
+
 # Run visor directly (running as root inside container is acceptable and avoids duplicate processes under Rosetta)
 exec gosu hluser /home/hluser/hl-visor run-non-validator --replica-cmds-style recent-actions --serve-eth-rpc 
